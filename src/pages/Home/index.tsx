e2da6b267834286d2eea69a630f94b52427e1a03
Cristian Araujo <araujocristian@live.com>
2022-08-19 21:32:50 -0300

    feat: Improve Inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,14 +18,25 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto1" />
+            <option value="Projeto2" />
+            <option value="Projeto3" />
+            <option value="Projeto4" />
+          </datalist>
 
           <label htmlFor="time">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={0}
+            max={60}
           />
 
           <span>minutos.</span>
